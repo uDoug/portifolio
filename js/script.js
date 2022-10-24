6f53ba5feb8e9609b26763b57b6c7cc9d7368230
Douglas Viera <douglasdamascena13@gmail.com>
2022-10-24 01:03:27 -0300

    falta img

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
-const header = document.querySelector.querySelector("header");
+const header = document.querySelector("header");
 
 window.addEventListener("scroll", function () {
     header.classList.toggle9("sticky", window.scrollY > 0);
 
 });
+
+let menu = document.querySelector('#navbarIcon');
+let navList = document.querySelector('.navbarHeader');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navList.classList.toggle('active');
+}
+
+const sr = Scrollreveal({
+    distance: '50px',
+    duration: 2500,
+    reset: true
+});
+
+sr.reveal('.homeText', { delay: 350, origin: 'left' })
